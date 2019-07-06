000289e6004c5de33ea5960b1bb9e80717d600c6
Rahul Kala <rahul@gmail.com>
2019-07-06 20:33:37 +0530

    Commit in my repo first

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,6 +3,7 @@ package package1;
 public class Class1 {
 	private String var1;
 	private String var2;
+	private String var3;
 	
 	public void setVar1() {
 		var1 = "This is var 1";
@@ -18,5 +19,13 @@ public class Class1 {
 	
 	public String getVar2() {
 		return var2;
+	}
+
+	public String getVar3() {
+		return var3;
+	}
+
+	public void setVar3(String var3) {
+		this.var3 = var3;
 	}
 }
