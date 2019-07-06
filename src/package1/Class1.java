fc3848e6f28a6d2f009690dc7bf98e5a628e7871
Ramakrishnan Iyer <iyerramakrishnan5@gmail.com>
2019-07-06 20:10:50 +0530

    First Commit

---
+++ src/package1/Class1.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Class1 {
+	private String var1;
+	private String var2;
+	
+	public void setVar1() {
+		var1 = "This is var 1";
+	}
+	
+	public void setVar2() {
+		var1 = "This is var 2";
+	}
+	
+	public String getVar1() {
+		return var1;
+	}
+	
+	public String getVar2() {
+		return var2;
+	}
+}
