6f3cd527fbaab021041b99dd2a73883e2d5fdbc1
Rahul Kala <rahul@gmail.com>
2019-07-06 20:36:55 +0530

    second commit to my local of git_tutorial

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,23 +2,23 @@ package package1;
 
 public class Class1 {
 	private String var1;
-	private String var2;
+	private String var6;
 	private String var3;
 	
 	public void setVar1() {
 		var1 = "This is var 1";
 	}
 	
-	public void setVar2() {
-		var1 = "This is var 2";
+	public void setVar6() {
+		var1 = "This is var 6";
 	}
 	
 	public String getVar1() {
 		return var1;
 	}
 	
-	public String getVar2() {
-		return var2;
+	public String getVar6() {
+		return var6;
 	}
 
 	public String getVar3() {
